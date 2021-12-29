5d750bea901f16126dc34808029d24324be3796f
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2021-12-29 10:28:17 +0100

    Update tmask_transporter.py

--- tmask_transporter.py
+++ tmask_transporter.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+__author__ = "biuro@tmask.pl"
+__copyright__ = "Copyright (C) 2021 TMask.pl"
+__license__ = "MIT License"
+__version__ = "1.0"
+
 import tmask_email
 import tmask_path
 import tmask_ssh
